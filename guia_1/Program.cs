3975bd3e855233aecc3e6d890ae9494b9f168c85
KimberlyZE <kzapatae@uamv.edu.ni>
2024-10-03 22:20:41 -0600

    Add project files.

---
+++ guia_1/Program.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    struct Software
+    {
+        public string Fabricante;
+        public string Nombre;
+        public string Edicion;
+        public string Version;
+        public string Licenciamiento;
+        public string Descripcion;
+    }
+
+    static void Main()
+    {
+        Software[] listaSoftware = new Software[3];
+        int totalRegistros = 0;
+        string archivo = "software.dat";
+
+        int opcion;
+        do
+        {
+            Console.WriteLine("\nMenu:");
+            Console.WriteLine("1. Agregar Software");
+            Console.WriteLine("2. Mostrar Software");
+            Console.WriteLine("3. Actualizar Software");
+            Console.WriteLine("4. Eliminar Software");
+            Console.WriteLine("5. Guardar en disco");
+            Console.WriteLine("6. Leer desde disco");
+            Console.WriteLine("0. Salir");
+            Console.Write("Ingrese una opción: ");
+            opcion = int.Parse(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1: // Agregar Software
+                    if (totalRegistros < listaSoftware.Length)
+                    {
+                        Console.WriteLine("Agregar Software:");
+                        Console.Write("Fabricante: ");
+                        listaSoftware[totalRegistros].Fabricante = Console.ReadLine();
+                        Console.Write("Nombre: ");
+                        listaSoftware[totalRegistros].Nombre = Console.ReadLine();
+                        Console.Write("Edición: ");
+                        listaSoftware[totalRegistros].Edicion = Console.ReadLine();
+                        Console.Write("Versión: ");
+                        listaSoftware[totalRegistros].Version = Console.ReadLine();
+                        Console.Write("Licenciamiento: ");
+                        listaSoftware[totalRegistros].Licenciamiento = Console.ReadLine();
+                        Console.Write("Descripción: ");
+                        listaSoftware[totalRegistros].Descripcion = Console.ReadLine();
+                        totalRegistros++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("No hay espacio para más registros.");
+                    }
+                    break;
+
+                case 2: // Mostrar Software
+                    Console.WriteLine("\nLista de Software:");
+                    for (int i = 0; i < totalRegistros; i++)
+                    {
+                        MostrarSoftware(listaSoftware[i]);
+                    }
+                    break;
+
+                case 3: // Actualizar Software
+                    Console.Write("Ingrese el número del registro a actualizar (0 a {0}): ", totalRegistros - 1);
+                    int indice = int.Parse(Console.ReadLine());
+                    if (indice >= 0 && indice < totalRegistros)
+                    {
+                        Console.WriteLine("Actualizar Software:");
+                        Console.Write("Nuevo Fabricante: ");
+                        listaSoftware[indice].Fabricante = Console.ReadLine();
+                        Console.Write("Nuevo Nombre: ");
+                        listaSoftware[indice].Nombre = Console.ReadLine();
+                        Console.Write("Nueva Edición: ");
+                        listaSoftware[indice].Edicion = Console.ReadLine();
+                        Console.Write("Nueva Versión: ");
+                        listaSoftware[indice].Version = Console.ReadLine();
+                        Console.Write("Nuevo Licenciamiento: ");
+                        listaSoftware[indice].Licenciamiento = Console.ReadLine();
+                        Console.Write("Nueva Descripción: ");
+                        listaSoftware[indice].Descripcion = Console.ReadLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Índice no válido.");
+                    }
+                    break;
+
+                case 4: // Eliminar Software
+                    Console.Write("Ingrese el número del registro a eliminar (0 a {0}): ", totalRegistros - 1);
+                    int eliminarIndice = int.Parse(Console.ReadLine());
+                    if (eliminarIndice >= 0 && eliminarIndice < totalRegistros)
+                    {
+                        for (int i = eliminarIndice; i < totalRegistros - 1; i++)
+                        {
+                            listaSoftware[i] = listaSoftware[i + 1];
+                        }
+                        totalRegistros--;
+                        Console.WriteLine("Registro eliminado.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Índice no válido.");
+                    }
+                    break;
+
+                case 5: // Guardar en disco
+                    GuardarEnDisco(listaSoftware, totalRegistros, archivo);
+                    Console.WriteLine("Datos guardados en disco.");
+                    break;
+
+                case 6: // Leer desde disco
+                    totalRegistros = LeerDesdeDisco(listaSoftware, archivo);
+                    Console.WriteLine("Datos cargados desde disco.");
+                    break;
+
+                case 0:
+                    Console.WriteLine("Saliendo...");
+                    break;
+
+                default:
+                    Console.WriteLine("Opción no válida.");
+                    break;
+            }
+        } while (opcion != 0);
+    }
+
+    static void MostrarSoftware(Software software)
+    {
+        Console.WriteLine("Fabricante: " + software.Fabricante);
+        Console.WriteLine("Nombre: " + software.Nombre);
+        Console.WriteLine("Edición: " + software.Edicion);
+        Console.WriteLine("Versión: " + software.Version);
+        Console.WriteLine("Licenciamiento: " + software.Licenciamiento);
+        Console.WriteLine("Descripción: " + software.Descripcion);
+        Console.WriteLine();
+    }
+
+    static void GuardarEnDisco(Software[] listaSoftware, int totalRegistros, string archivo)
+    {
+        FileStream fileStream = new FileStream(archivo, FileMode.OpenOrCreate, FileAccess.Write);
+        BinaryWriter writer = new BinaryWriter(fileStream);
+
+        writer.Write(totalRegistros); // Guardamos la cantidad de registros
+        for (int i = 0; i < totalRegistros; i++)
+        {
+            writer.Write(listaSoftware[i].Fabricante);
+            writer.Write(listaSoftware[i].Nombre);
+            writer.Write(listaSoftware[i].Edicion);
+            writer.Write(listaSoftware[i].Version);
+            writer.Write(listaSoftware[i].Licenciamiento);
+            writer.Write(listaSoftware[i].Descripcion);
+        }
+
+        writer.Close();
+        fileStream.Close();
+    }
+
+    static int LeerDesdeDisco(Software[] listaSoftware, string archivo)
+    {
+        if (File.Exists(archivo))
+        {
+            FileStream fileStream = new FileStream(archivo, FileMode.Open, FileAccess.Read);
+            BinaryReader reader = new BinaryReader(fileStream);
+
+            int totalRegistros = reader.ReadInt32(); // Leemos la cantidad de registros
+            for (int i = 0; i < totalRegistros; i++)
+            {
+                listaSoftware[i].Fabricante = reader.ReadString();
+                listaSoftware[i].Nombre = reader.ReadString();
+                listaSoftware[i].Edicion = reader.ReadString();
+                listaSoftware[i].Version = reader.ReadString();
+                listaSoftware[i].Licenciamiento = reader.ReadString();
+                listaSoftware[i].Descripcion = reader.ReadString();
+            }
+
+            reader.Close();
+            fileStream.Close();
+            return totalRegistros;
+        }
+        else
+        {
+            Console.WriteLine("El archivo no existe.");
+            return 0;
+        }
+    }
+}
+
+
---
+++ guia_dos/Program.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    struct Estudiante
+    {
+        public string Nombre;
+        public int IdEstudiante;
+    }
+
+    struct Asignatura
+    {
+        public string Nombre;
+        public int IdAsignatura;
+    }
+
+    struct Calificacion
+    {
+        public int IdEstudiante;
+        public int IdAsignatura;
+        public float Nota;
+    }
+
+    static Estudiante[] listaEstudiantes = new Estudiante[3];
+    static Asignatura[] listaAsignaturas = new Asignatura[3];
+    static Calificacion[] listaCalificaciones = new Calificacion[9];
+    static int totalEstudiantes = 0;
+    static int totalAsignaturas = 0;
+    static int totalCalificaciones = 0;
+
+    static string archivo = "calificaciones.dat";
+
+    static void Main(string[] args)
+    {
+        int opcion;
+        do
+        {
+            Console.WriteLine("\nMenu:");
+            Console.WriteLine("1. Agregar Estudiante");
+            Console.WriteLine("2. Agregar Asignatura");
+            Console.WriteLine("3. Agregar Calificación");
+            Console.WriteLine("4. Mostrar Calificaciones");
+            Console.WriteLine("5. Guardar en Disco");
+            Console.WriteLine("6. Leer desde Disco");
+            Console.WriteLine("0. Salir");
+            Console.Write("Ingrese una opción: ");
+            opcion = int.Parse(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    AgregarEstudiante();
+                    break;
+
+                case 2:
+                    AgregarAsignatura();
+                    break;
+
+                case 3:
+                    AgregarCalificacion();
+                    break;
+
+                case 4:
+                    MostrarCalificaciones();
+                    break;
+
+                case 5:
+                    GuardarEnDisco();
+                    Console.WriteLine("Datos guardados en disco.");
+                    break;
+
+                case 6:
+                    LeerDesdeDisco();
+                    Console.WriteLine("Datos cargados desde disco.");
+                    break;
+
+                case 0:
+                    Console.WriteLine("Saliendo...");
+                    break;
+
+                default:
+                    Console.WriteLine("Opción no válida.");
+                    break;
+            }
+        } while (opcion != 0);
+    }
+
+    static void AgregarEstudiante()
+    {
+        if (totalEstudiantes < listaEstudiantes.Length)
+        {
+            Console.Write("Ingrese el nombre del estudiante: ");
+            listaEstudiantes[totalEstudiantes].Nombre = Console.ReadLine();
+            listaEstudiantes[totalEstudiantes].IdEstudiante = totalEstudiantes + 1;
+            totalEstudiantes++;
+        }
+        else
+        {
+            Console.WriteLine("No hay espacio para más estudiantes.");
+        }
+    }
+
+    static void AgregarAsignatura()
+    {
+        if (totalAsignaturas < listaAsignaturas.Length)
+        {
+            Console.Write("Ingrese el nombre de la asignatura: ");
+            listaAsignaturas[totalAsignaturas].Nombre = Console.ReadLine();
+            listaAsignaturas[totalAsignaturas].IdAsignatura = totalAsignaturas + 1;
+            totalAsignaturas++;
+        }
+        else
+        {
+            Console.WriteLine("No hay espacio para más asignaturas.");
+        }
+    }
+
+    static void AgregarCalificacion()
+    {
+        if (totalCalificaciones < listaCalificaciones.Length)
+        {
+            Console.WriteLine("\nEstudiantes:");
+            for (int i = 0; i < totalEstudiantes; i++)
+            {
+                Console.WriteLine($"{listaEstudiantes[i].IdEstudiante}. {listaEstudiantes[i].Nombre}");
+            }
+            Console.Write("Seleccione el ID del estudiante: ");
+            listaCalificaciones[totalCalificaciones].IdEstudiante = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nAsignaturas:");
+            for (int i = 0; i < totalAsignaturas; i++)
+            {
+                Console.WriteLine($"{listaAsignaturas[i].IdAsignatura}. {listaAsignaturas[i].Nombre}");
+            }
+            Console.Write("Seleccione el ID de la asignatura: ");
+            listaCalificaciones[totalCalificaciones].IdAsignatura = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese la calificación: ");
+            listaCalificaciones[totalCalificaciones].Nota = float.Parse(Console.ReadLine());
+
+            totalCalificaciones++;
+        }
+        else
+        {
+            Console.WriteLine("No hay espacio para más calificaciones.");
+        }
+    }
+
+    static void MostrarCalificaciones()
+    {
+        Console.WriteLine("\nLista de Calificaciones:");
+        for (int i = 0; i < totalCalificaciones; i++)
+        {
+            string nombreEstudiante = listaEstudiantes[listaCalificaciones[i].IdEstudiante - 1].Nombre;
+            string nombreAsignatura = listaAsignaturas[listaCalificaciones[i].IdAsignatura - 1].Nombre;
+            Console.WriteLine($"Estudiante: {nombreEstudiante}, Asignatura: {nombreAsignatura}, Nota: {listaCalificaciones[i].Nota}");
+        }
+    }
+
+    static void GuardarEnDisco()
+    {
+        FileStream fileStream = new FileStream(archivo, FileMode.OpenOrCreate, FileAccess.Write);
+        BinaryWriter writer = new BinaryWriter(fileStream);
+
+        writer.Write(totalEstudiantes);
+        for (int i = 0; i < totalEstudiantes; i++)
+        {
+            writer.Write(listaEstudiantes[i].Nombre);
+            writer.Write(listaEstudiantes[i].IdEstudiante);
+        }
+
+        writer.Write(totalAsignaturas);
+        for (int i = 0; i < totalAsignaturas; i++)
+        {
+            writer.Write(listaAsignaturas[i].Nombre);
+            writer.Write(listaAsignaturas[i].IdAsignatura);
+        }
+
+        writer.Write(totalCalificaciones);
+        for (int i = 0; i < totalCalificaciones; i++)
+        {
+            writer.Write(listaCalificaciones[i].IdEstudiante);
+            writer.Write(listaCalificaciones[i].IdAsignatura);
+            writer.Write(listaCalificaciones[i].Nota);
+        }
+
+        writer.Close();
+        fileStream.Close();
+    }
+
+    static void LeerDesdeDisco()
+    {
+        if (File.Exists(archivo))
+        {
+            FileStream fileStream = new FileStream(archivo, FileMode.Open, FileAccess.Read);
+            BinaryReader reader = new BinaryReader(fileStream);
+
+            totalEstudiantes = reader.ReadInt32();
+            for (int i = 0; i < totalEstudiantes; i++)
+            {
+                listaEstudiantes[i].Nombre = reader.ReadString();
+                listaEstudiantes[i].IdEstudiante = reader.ReadInt32();
+            }
+
+            totalAsignaturas = reader.ReadInt32();
+            for (int i = 0; i < totalAsignaturas; i++)
+            {
+                listaAsignaturas[i].Nombre = reader.ReadString();
+                listaAsignaturas[i].IdAsignatura = reader.ReadInt32();
+            }
+
+            totalCalificaciones = reader.ReadInt32();
+            for (int i = 0; i < totalCalificaciones; i++)
+            {
+                listaCalificaciones[i].IdEstudiante = reader.ReadInt32();
+                listaCalificaciones[i].IdAsignatura = reader.ReadInt32();
+                listaCalificaciones[i].Nota = reader.ReadSingle();
+            }
+
+            reader.Close();
+            fileStream.Close();
+        }
+        else
+        {
+            Console.WriteLine("El archivo no existe.");
+        }
+    }
+}
+
+
